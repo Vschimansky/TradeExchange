44f15a115274b7d2779244919371397a2f57d95b
Jose M. Luis <jmluismoreno@gmail.com>
2020-08-28 15:06:55 -0400

    Added base spring

---
+++ src/main/java/com/conygre/spring/SpringRESTApi/SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.conygre.spring.SpringRESTApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conygre/spring/SpringRESTApi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conygre.spring.SpringRESTApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
